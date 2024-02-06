bd49917f866ea60c394882d9ea1a196c3d8f108e
liem.dinh <liem.dinh@deltabrains.vn>
2024-02-06 17:37:42 +0700

    Task 1: FE integrate api with vnpay service

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Footer from "./components/Footer";
 import Profile from "./pages/user/Profile";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
+import Payment from "./pages/payment/Payment";
 
 // check Role
 import { userSection } from "./utils/checkRole";
@@ -20,29 +21,30 @@ import ManageOrders from "./pages/ordermanagement/OrderManage";
 import ManageUsers from "./pages/manage-user/ManageUser";
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<Navbar />
-			<Routes>
-				<Route index element={<Home />} />
-				<Route path="about" element={<About />} />
-				<Route path="contact" element={<Contact />} />
-				<Route path="gallery" element={<Gallery />} />
-				<Route path="plans" element={<Plans />} />
-				<Route path="trainers" element={<Trainers />} />
-				{userSection && <Route path="profile/:id" element={<Profile />} />}
-				{userSection?.role === 'admin' && <Route path="manage-customers" element={< ManageOrders />} />}
-				{userSection?.role === 'admin' && <Route path="manage-users" element={< ManageUsers />} />}
-				{userSection?.role === 'admin' && <Route path="manage-orders" element={<ManageOrders />} />}
-				{userSection?.role === 'pt' && <Route path="manage-customers" element={<h1 className="text-[180px]">Manage customer registed on PT ROLE</h1>} />}
-				{userSection?.role === 'customer' && <Route path="manage-pts" element={<h1 className="text-[180px]">Manage customer registed on Customer ROLE</h1>} />}
-				<Route path="login" element={<Login />} />
-				<Route path="signup" element={<SignUp />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-			<Footer />
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="gallery" element={<Gallery />} />
+        <Route path="plans" element={<Plans />} />
+        <Route path="trainers" element={<Trainers />} />
+        {userSection && <Route path="profile/:id" element={<Profile />} />}
+        {userSection?.role === 'admin' && <Route path="manage-customers" element={< ManageOrders />} />}
+        {userSection?.role === 'admin' && <Route path="manage-users" element={< ManageUsers />} />}
+        {userSection?.role === 'admin' && <Route path="manage-orders" element={<ManageOrders />} />}
+        {userSection?.role === 'pt' && <Route path="manage-customers" element={<h1 className="text-[180px]">Manage customer registed on PT ROLE</h1>} />}
+        {userSection?.role === 'customer' && <Route path="manage-pts" element={<h1 className="text-[180px]">Manage customer registed on Customer ROLE</h1>} />}
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="payment/callback" element={<Payment />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/payment/Payment.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getPaymentResult } from '../../api/invoiceService';
+import './payment.css';
+
+export const Payment = () => {
+  const [showDialog, setShowDialog] = useState(true);
+  const [modalTitle, setModalTitle] = useState('');
+  const [modalBody, setModalBody] = useState('');
+  const [queryResult, setQueryResult] = useState({});
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    getQueryResult();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (Object.keys(queryResult).length !== 0) {
+      fetchResultPayment();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [queryResult]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const fetchResultPayment = async () => {
+    const getInfoUser = JSON.parse(sessionStorage.getItem('user'));
+    const userId = getInfoUser.userId;
+    const res = await getPaymentResult(queryResult, userId);
+    if (res.RspCode === '00') {
+      setModalTitle('Success');
+      setModalBody('Payment success!');
+    } else {
+      setModalTitle('Error');
+      res.RspCode === '99'
+        ? setModalBody('Payment existed!')
+        : setModalBody('Payment failed!');
+    }
+    setShowDialog(true);
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const getQueryResult = () => {
+    const vpnReturn = 'http://localhost:3000/payment/callback?';
+    const currentUrl = window.location.href;
+
+    const query = currentUrl.replace(vpnReturn, '');
+    const params = new URLSearchParams(query);
+    const result = {};
+    params.forEach((value, key) => {
+      if (value.includes('%')) {
+        // Decode encoded values
+        value = decodeURIComponent(value);
+      }
+      result[key] = value;
+    });
+    setQueryResult(result);
+  };
+
+  return showDialog ? (
+    <section>
+      <div className='container notify__container py-6'>
+        <div className='text-center'>
+          <h2> {modalTitle} </h2>
+          <span>{modalBody}</span>
+        </div>
+        <Link to='/' className='btn'>
+          Go back Home
+        </Link>
+      </div>
+    </section>
+  ) : (
+    <section>
+      <div className='text-center py-6'>
+        <h2> Loading.... </h2>
+      </div>
+    </section>
+  );
+};
+
+export default Payment;
